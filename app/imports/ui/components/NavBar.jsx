d2780354e6663f6c0abc9c25722957297daa54df
Anson2Leung <125102097+Anson2Leung@users.noreply.github.com>
2023-11-14 10:19:05 -1000

    NavBar.jsx - Containers and Full Cycle Takeout

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -3,7 +3,7 @@ import { Meteor } from 'meteor/meteor';
 import { useTracker } from 'meteor/react-meteor-data';
 import { NavLink } from 'react-router-dom';
 import { Roles } from 'meteor/alanning:roles';
-import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { Container, Nav, Navbar, NavDropdown, Image } from 'react-bootstrap';
 import { BoxArrowRight, PersonFill, PersonPlusFill } from 'react-bootstrap-icons';
 
 const NavBar = () => {
@@ -16,11 +16,13 @@ const NavBar = () => {
     <Navbar bg="light" expand="lg">
       <Container>
         <Navbar.Brand as={NavLink} to="/">
-          <h2>meteor-application-template-react</h2>
+          <Image src="https://cdn.discordapp.com/attachments/1169783962864988201/1171205404659810476/HACC_-_ZERO_WASTE_1.png?ex=655bd508&is=65496008&hm=767e27962275a795b00a4681f65f526774fad684a28ecb26599a0290019c8858&" width="150px" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto justify-content-start">
+            <Nav.Link className="px-5" href="https://www.fullcycletakeouthawaii.org/" target="_blank">FULL CYCLE TAKEOUT </Nav.Link>
+            <Nav.Link id="landing" as={NavLink} to="/" key="">Containers</Nav.Link>
             {currentUser ? ([
               <Nav.Link id="add-stuff-nav" as={NavLink} to="/add" key="add">Add Stuff</Nav.Link>,
               <Nav.Link id="list-stuff-nav" as={NavLink} to="/list" key="list">List Stuff</Nav.Link>,
