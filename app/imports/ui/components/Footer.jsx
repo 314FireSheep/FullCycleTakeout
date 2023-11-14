2e22be90e2e702ecb98fd1e63836e4f7a5ef0289
Anson2Leung <125102097+Anson2Leung@users.noreply.github.com>
2023-11-14 11:19:43 -1000

    Footer.jsx - Replaced footer

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -1,26 +1,29 @@
 import React from 'react';
-import { Col, Container } from 'react-bootstrap';
+import { Col, Container, Nav, Row } from 'react-bootstrap';
+import { Instagram } from 'react-bootstrap-icons';
 
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 const Footer = () => (
-  <footer className="mt-auto py-3 bg-light">
-    <Container>
-      <Col className="text-center">
-        Department of Information and Computer Sciences
-        {' '}
-        <br />
-        University of Hawaii
-        <br />
-        Honolulu, HI 96822
-        {' '}
-        <br />
-        <a href="http://ics-software-engineering.github.io/meteor-application-template-react">
-          Template Home
-          Page
-        </a>
+  <Container fluid expand="xl" className="bg-dark py-4">
+    <Row className="justify-content-center">
+      <Col className="justify-content-center ps-5">
+        <h5 className="text-white">About Us</h5>
+        <Nav.Link href="https://www.zerowasteoahu.org/" target="_blank">Zero Waste Oahu</Nav.Link>
+        <Nav className="text-white">Page Was Produced By FireSheep</Nav>
       </Col>
-    </Container>
-  </footer>
+      <Col>
+        <h5 className="text-white">Resources</h5>
+        <Nav.Link href="https://www.zerowasteoahu.org/what-we-do" target="_blank">About Zero Waste Oahu</Nav.Link>
+        <Nav.Link href="https://www.zerowasteoahu.org/contact" target="_blank">Contact Zero Waste Oahu</Nav.Link>
+        <Nav.Link href="https://www.zerowasteoahu.org/overview" target="_blank">Zero Waste Oahu Resources</Nav.Link>
+      </Col>
+      <Col className="justify-content-center">
+        <h5 className="text-white">Contacts</h5>
+        <Nav.Link className="white" href="https://www.instagram.com/zerowaste_oahu/?igshid=MTJxY3NydTdxN3M0MA%3D%3D" target="_blank"><Instagram /></Nav.Link>
+        <Nav className="text-white text-center">OAHU.ZEROWASTE@GMAIL.COM</Nav>
+      </Col>
+    </Row>
+  </Container>
 );
 
 export default Footer;
