9d05d4babc20ac3449e4de74c05e5365b9405d50
Brian Shu <110252035+shu4dev@users.noreply.github.com>
2023-11-26 01:36:37 -1000

    update

--- app/imports/api/order/Order.js
+++ app/imports/api/order/Order.js
@@ -10,19 +10,6 @@ class OrdersCollection {
     this.name = 'OrdersCollection';
     // Define the Mongo collection.
     this.collection = new Mongo.Collection(this.name);
-    // Define the structure of each document in the collection.
-    this.schema = new SimpleSchema({
-      name: String,
-      quantity: Number,
-      owner: String,
-      condition: {
-        type: String,
-        allowedValues: ['excellent', 'good', 'fair', 'poor'],
-        defaultValue: 'good',
-      },
-    });
-    // Attach the schema to the collection, so all attempts to insert a document are checked against schema.
-    this.collection.attachSchema(this.schema);
     // Define names for ublications and subscriptions
     this.userPublicationName = `${this.name}.publication.user`;
     this.adminPublicationName = `${this.name}.publication.admin`;
